30e4e8ff0311d13c53f6311bdfcebeef0541169a
Charles Ofuasia <ofu23001@byui.edu>
2023-09-22 11:30:20 +0100

    Display for the home page modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,4 +6,5 @@ document.querySelector("#copyDate").textContent = year;
 
 //codes to get the time and date a file or document was last updated.
 const mDate = new Date(document.lastModified);
+
 document.querySelector("#lastModified").innerHTML = mDate;
