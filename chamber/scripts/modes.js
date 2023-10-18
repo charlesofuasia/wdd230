ac17dd80a05333b3371b8c068708e530b6ae1c08
Charles Ofuasia <ofu23001@byui.edu>
2023-10-18 15:24:41 +0100

    Changed icon for dark mode to sunglasses

--- chamber/scripts/modes.js
+++ chamber/scripts/modes.js
@@ -2,13 +2,13 @@ const modeBtn = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeBtn.addEventListener("click", () =>{
-    if (modeBtn.textContent.includes("🌙")){
+    if (modeBtn.textContent.includes("🕶️")){
         main.style.backgroundColor = "#000"
         main.style.color = "#fff"
         modeBtn.textContent = "🔆"
     }
     else{
-        modeBtn.textContent = "🌙";
+        modeBtn.textContent = "🕶️";
         main.style.backgroundColor = "#c2ffc2"
         main.style.color = "#000"
     }
