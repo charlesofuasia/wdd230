a1655be07f94c15e4811af508de5893d3265f6b4
Charles Ofuasia <ofu23001@byui.edu>
2023-10-28 14:44:39 +0100

    Updated to count difference between days.

--- chamber/scripts/visitor.js
+++ chamber/scripts/visitor.js
@@ -1,13 +1,34 @@
 const numVisit = document.querySelector("#visits");
+//convert day into millisecond.
+const dayInMsecs = 1000*60*60*24; 
+
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));//get lastVisit from local storage, change from string to number.
+let presentTime = Date.now();//gives present time in milliseconds.
+let timeDifference = (presentTime - lastVisit)/ dayInMsecs;
+
+
 
 let visitCount = parseInt(window.localStorage.getItem("pagevisit-ls")) || 0;
 
-if (visitCount != 0){
-    numVisit.textContent = `Your number of visits here is: ${visitCount}`;
-}else{
+if (visitCount == 0){
     numVisit.textContent = "Welcome to my page! your fist visit";
-}
+}else if (timeDifference < 1) {
+    numVisit.textContent = "Awesome! Back so soon"
+
+}else{
+    numVisit.textContent = `You visited this side ${timeDifference} day${addS(timeDifference)} ago.`;
+};
 
 visitCount++
 
-localStorage.setItem("pagevisit-ls", visitCount);
+localStorage.setItem("lastVisit", presentTime);
+localStorage.setItem("pagevisits-ls", visitCount);
+
+//function to add letter "s" to days if days are greater than 1.
+function addS(timeDifference){
+    if (timeDifference > 1){
+        return "s";
+    }else{
+        return "";
+    }
+}
