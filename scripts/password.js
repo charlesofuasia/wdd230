3f5725c89a605029f04622851c7b3f24c5a64e9b
Charles Ofuasia <ofu23001@byui.edu>
2023-11-02 20:44:12 +0100

    form design done.

---
+++ scripts/password.js
@@ -0,0 +1,16 @@
+const firstPassword = document.querySelector('#pw1');
+const secondPassword = document.querySelector('#pw2');
+const message = document.querySelector('#message');
+
+secondPassword.addEventListener("focusout", checkPassword);
+
+function checkPassword(){
+    if (secondPassword.value != firstPassword.value){
+        message.innerHTML = "Passwords are not the same.";
+        secondPassword.innerHTML ="";
+        secondPassword.blur();
+    }else{
+        message.innerHTML = "";
+    }
+    secondPassword.focus();
+};
