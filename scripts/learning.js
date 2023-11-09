beb0b1102b7961df3bee6b6ff8c09df6cb215452
Charles Ofuasia <ofu23001@byui.edu>
2023-11-09 23:04:04 +0100

    assignment listed with json

---
+++ scripts/learning.js
@@ -0,0 +1,24 @@
+const activityList = document.querySelector('#learning-list');
+const url = "https://charlesofuasia.github.io/wdd230/learning.json";
+
+async function getList(){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    loadList(data.learningActivities);
+}
+
+const loadList = (learningActivities) => {
+    learningActivities.forEach((activity) => {
+        let list = document.createElement('li');
+        let link = document.createElement('a');
+        link.textContent = activity.name;
+        link.href = activity.weblink;
+        list.appendChild(link);
+        activityList.append(list);
+       
+        activityList.append(list);
+    
+    })
+}
+getList()
