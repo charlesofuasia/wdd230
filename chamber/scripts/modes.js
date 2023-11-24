a9b893dbe21e44209428938cd26a99d45463bb59
Charles Ofuasia <ofu23001@byui.edu>
2023-11-24 14:48:55 +0100

    added dark mode effect to header

--- chamber/scripts/modes.js
+++ chamber/scripts/modes.js
@@ -1,15 +1,20 @@
 const modeBtn = document.querySelector("#mode");
 const main = document.querySelector("main");
+const header = document.querySelector("header")
 
 modeBtn.addEventListener("click", () =>{
     if (modeBtn.textContent.includes("🕶️")){
-        main.style.backgroundColor = "#000"
-        main.style.color = "#fff"
-        modeBtn.textContent = "🔆"
+        main.style.backgroundColor = "#000";
+        main.style.color = "#fff";
+        header.style.backgroundColor = "#000";
+        header.style.color = "#fff";
+        modeBtn.textContent = "🔆";
     }
     else{
         modeBtn.textContent = "🕶️";
-        main.style.backgroundColor = "#c2ffc2"
-        main.style.color = "#000"
+        main.style.backgroundColor = "#c2ffc2";
+        main.style.color = "#000";
+        header.style.backgroundColor = "#c2ffc2";
+        header.style.color = "#140014"
     }
 })
