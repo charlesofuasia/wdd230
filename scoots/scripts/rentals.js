58f76ecb7481bb56e5667be7834c2eab568340eb
Charles Ofuasia <ofu23001@byui.edu>
2023-12-10 18:43:01 +0100

    rentals details added

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,18 +1,63 @@
 const url = "data/rentals.json"
-const ren = document.querySelector('rentals');
+const cards = document.querySelector('#rentals');
 
 async function fetchRentals(){
     const response = await fetch(url)
     if (response.ok){
         const data = await response.json();
-        console.log(data.rentals)
+        loadRentals(data.rentals)
     }
 }
 
-function loadRentals(rentals){
-   const photo = document.createElement('h3');
-   photo.textContent = rentals[0].name;
-   ren.append(photo);
+function loadRentals(data){
+    data.forEach((rental) => {
+        let card = document.createElement('div');
+        let title = document.createElement('h3');
+        let photo = document.createElement('img');
+        let table = document.createElement('table');
+        let details = document.createElement('p');
+
+        title.textContent = rental.name;
+        photo.src = rental.imageurl;
+        photo.alt = `Photo of ${rental.name}`;
+        photo.width = "500";
+        photo.height = "400";
+        photo.loading = "lazy";
+        table.innerHTML = `  <thead>
+        <tr>
+          <th colspan="3" scope="colgroup">Rental Pricing</th>
+        </tr>
+        <tr>
+          <th scope="col">Customer Type</th>
+          <th scope="col">Half Day</th>
+          <th scope="col">Full Day</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Reservation</th>
+          <td>${rental.rehalf}</td>
+          <td>${rental.refull}</td>
+        </tr>
+        <tr>
+          <th scope="row">Walk-In</th>
+          <td>${rental.inhalf}</td>
+          <td>${rental.infull}</td>
+        </tr>
+      </tbody>`;
+      details.innerHTML = `${rental.description} Max-Capacity: ${rental.capacity}`;
+
+
+      card.appendChild(title);
+      card.appendChild(photo);
+      card.appendChild(table);
+      card.appendChild(details);
+
+      cards.append(card);
+
+        
+        
+    });
 }
 
 fetchRentals();
