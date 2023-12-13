c32cf50b0a2a6aadd0681390e9ddaab9a4e89ea5
Charles Ofuasia <ofu23001@byui.edu>
2023-12-13 20:19:11 +0100

    cleaning up the css

--- scoots/scripts/dates.js
+++ scoots/scripts/dates.js
@@ -3,5 +3,4 @@ let information = document.querySelector('#info');
 let dt = new Date();
 let yr = dt.getFullYear();
 
-information.innerHTML = `&copy; ${yr} Scoots Rental Conzumel Mexico <br>
-<a href="attribution.html">Attributions</a>`
+information.innerHTML = `&copy; ${yr} Scoots Rental, Conzumel Mexico`
