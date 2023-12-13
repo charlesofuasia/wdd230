08eb954467e68cc374518942cd01773ddb4bf48e
Charles Ofuasia <ofu23001@byui.edu>
2023-12-13 09:28:19 +0100

    starting footer contents

---
+++ scoots/scripts/dates.js
@@ -0,0 +1,7 @@
+let information = document.querySelector('#info');
+
+let dt = new Date();
+let yr = dt.getFullYear();
+
+information.innerHTML = `&copy; ${yr} Scoots Rental Conzumel Mexico <br>
+<a href="attribution.html">Attributions</a>`
